d6489418cc35a9ba4a51e84cf29399dc7ecbfa25
Unknown <neildidi33@gmail.com>
2017-12-13 12:43:37 +0100

    Script camera & Mouvement

--- Assets/Projet_Katamari/Script/CompleteCameraController.cs
+++ Assets/Projet_Katamari/Script/CompleteCameraController.cs
@@ -9,6 +9,8 @@ public class CompleteCameraController : MonoBehaviour
 
     public GameObject player;       //Public variable to store a reference to the player game object
 
+    public float _speedRotationCamera = 2.0f;
+
     #endregion
 
     #region Publuc void
@@ -40,19 +42,28 @@ public class CompleteCameraController : MonoBehaviour
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         m_transform.position = player.transform.position + offset;
+
+        _gaucheDroite += _speedRotationCamera * Input.GetAxis("Mouse X");
+
+        m_transform.eulerAngles = new Vector3(_gaucheDroite, 0.0f, 0.0f);
+        player.transform.Rotate(m_transform.eulerAngles);
+
     }
 
     #endregion
 
-    #region Tools Debug and Utility
+        #region Tools Debug and Utility
 
-    #endregion
+        #endregion
 
-    #region Private and Protected Members
+        #region Private and Protected Members
 
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
     private Transform m_transform;
 
+    //les noms des variables que j'ai repris d'un autre script
+    private float _gaucheDroite = 0.0f;
+  
 
     #endregion
 }
---
+++ Assets/Projet_Katamari/Script/Roulade.cs
@@ -0,0 +1,70 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roulade : MonoBehaviour
+{
+    #region Public Members
+
+    public float _speed;
+
+    #endregion
+
+    #region Publuc void
+
+    #endregion
+
+    #region system
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_transform = GetComponent<Transform>();
+
+    }
+
+    void Start () //pas de logique dans start et update
+    {
+		
+	}
+	
+	
+	void Update () 
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float _moveHorizontal = Input.GetAxis("Horizontal");
+        float _moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 _movement = new Vector3(_moveHorizontal, 0.0f, _moveVertical);
+
+        m_rigidbody.AddForce(_movement, ForceMode.VelocityChange);
+        //m_rigidbody.AddRelativeForce(_movement*_speed);
+    }
+
+
+    void OnTriggerEnter(Collider other)//à faire avec des layerMasks??
+    {
+        if (other.gameObject.CompareTag("Pick Up"))//à changer
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    private Rigidbody m_rigidbody;
+    private Transform m_transform;
+
+    #endregion
+}
