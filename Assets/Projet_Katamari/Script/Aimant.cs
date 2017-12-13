355ff7ab49ad7bf822afc2fc93233ad8f94ee43a
Unknown <neildidi33@gmail.com>
2017-12-13 17:30:33 +0100

    On rentre chez soiiiiii

---
+++ Assets/Projet_Katamari/Script/Aimant.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aimant : MonoBehaviour
+{
+    #region Public Members
+
+    public BoxCollider cube1;
+    public Transform player;
+
+    #endregion
+
+    #region Publuc void
+
+    #endregion
+
+    #region system
+
+    void Start () //pas de logique dans start et update
+    {
+            
+	}
+	
+	
+	void Update () 
+    {
+		
+	}
+
+    private void OnCollisionEnter(Collision cube1)
+    {
+        
+
+        Debug.Log("Collsion");
+
+        //TODO TOmorrow
+        //cube1.transform.parent= (player);
+    }
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    private float m_myFirstFloat;
+
+    #endregion
+}
