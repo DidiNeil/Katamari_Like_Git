ef2725925383134294881a6b064954d1eb525e41
Francois19 <francoisjanssens.pro@gmail.com>
2017-12-14 11:00:48 +0100

    Bulk commit (very bad :))

--- Assets/Projet_Katamari/Script/Aimant.cs
+++ Assets/Projet_Katamari/Script/Aimant.cs
@@ -7,8 +7,9 @@ public class Aimant : MonoBehaviour
 {
     #region Public Members
 
-    public BoxCollider cube1;
-    public Transform player;
+    public Transform cube1;
+    public int Score = 0;
+
 
     #endregion
 
@@ -17,6 +18,11 @@ public class Aimant : MonoBehaviour
     #endregion
 
     #region system
+
+    private void Awake()
+    {
+        m_transform = transform.GetComponent<Transform>();
+    }
 
     void Start () //pas de logique dans start et update
     {
@@ -28,12 +34,25 @@ public class Aimant : MonoBehaviour
     {
 		
 	}
+    public void SetParams(Transform root, int score) {
+        this.Score = score;
+        this.cube1 = root;
+    }
 
-    private void OnCollisionEnter(Collision cube1)
+    private void OnCollisionEnter (Collision collision)
     {
-        
 
-        Debug.Log("Collsion");
+        if (collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("Collision");
+            cube1.transform.parent = collision.transform;
+            RemoveColliders(cube1.gameObject);
+
+            //cube1.transform.GetComponent<BoxCollider>().isTrigger = true;
+            Score++;
+                
+
+        }
 
         //TODO TOmorrow
         //cube1.transform.parent= (player);
@@ -47,7 +66,29 @@ public class Aimant : MonoBehaviour
 
     #region Private and Protected Members
 
-    private float m_myFirstFloat;
+    private Transform m_transform;
+    private Rigidbody m_rigidbody;
 
     #endregion
+
+    public static void MakeItGlue(GameObject target, int score =5) {
+
+        Aimant createdscript= target.AddComponent<Aimant>();
+        RemoveColliders(target);
+        createdscript.SetParams(target.transform, score);
+    }
+
+
+    public static void RemoveColliders(GameObject target)
+    {
+            Destroy(target.transform.GetComponent<Rigidbody>());
+            //Supprimer le rigidbody
+            Destroy(target.transform.GetComponent<BoxCollider>());
+            //Supprimer les colliders
+        
+
+        //target.GetComponents<Collider>();
+        //target.GetComponentsInChildren<Collider>();
+        //Destroy(collider);
+    }
 }
---
+++ Assets/Projet_Katamari/Script/ObjectFactory.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFactory : MonoBehaviour 
+{
+
+
+    public GameObject[] _objectToCreated;
+    public Transform _where;
+    public float _timeBetweenCreatoin=2;
+
+
+
+    void Start() {
+
+
+        InvokeRepeating("CreateOnOfTheObject", 0, _timeBetweenCreatoin);
+    }
+
+
+
+    void CreateOnOfTheObject() {
+
+        GameObject obj = _objectToCreated[UnityEngine.Random.Range(0, _objectToCreated.Length - 1)];
+        GameObject created = Instantiate(obj, _where);
+        Aimant.MakeItGlue(created);
+    }
+
+	#region Public Members
+
+	// Les publics en début de code : car un autre dev en a besoin
+
+	#endregion
+
+	#region Public void
+
+	#endregion
+
+	#region System
+
+	
+    void Update () 
+	{
+        
+    }
+
+    #endregion
+
+    #region Tools Debug And Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    // Les privates en fin de code : car un autre dev n'en a pas besoin
+
+    #endregion
+}
